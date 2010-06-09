6b86bb55f504042745ca88e605af419ce0dc6aee
Scott Beardsley <scott@cse.ucdavis.edu>
2010-06-09 14:26:03 -0700

    oops, small change to exp backoff. start at 0

--- cobbler/action_power.py
+++ cobbler/action_power.py
@@ -112,7 +112,7 @@ class PowerTool:
             if rc == 0:
                 break
             else:
-                time.sleep(pow(2,x)) # do a lil exponential backoff :)
+                time.sleep(pow(2,x-1)) # do a lil exponential backoff :)
 
         if not rc == 0:
            utils.die(self.logger,"command failed (rc=%s), please validate the physical setup and cobbler config" % rc)
